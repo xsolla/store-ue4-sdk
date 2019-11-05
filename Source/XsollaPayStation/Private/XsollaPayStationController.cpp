0abb9bde21780836c253986197c352bad0328f02
Ivan Tustanivskyi <itustanivsky@ninevastudios.com>
2019-11-05 13:53:36 +0200

    SDK-85 Fix default PayStation settings values

--- Source/XsollaPayStation/Private/XsollaPayStationController.cpp
+++ Source/XsollaPayStation/Private/XsollaPayStationController.cpp
@@ -71,7 +71,7 @@ void UXsollaPayStationController::FetchPaymentToken_HttpRequestComplete(FHttpReq
 	else
 	{
 		UE_LOG(LogXsollaPayStation, Warning, TEXT("%s: request failed"), *VA_FUNC_LINE);
-		ErrorCallback.ExecuteIfBound(204, 0, TEXT("No response"));
+		ErrorCallback.ExecuteIfBound(HttpResponse->GetResponseCode(), 0, TEXT("No response"));
 	}
 }
 
