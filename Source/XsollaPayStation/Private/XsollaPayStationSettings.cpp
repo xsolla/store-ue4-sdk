af72168e7482659e3933236853cab77838e5c781
Ivan Tustanivskyi <itustanivsky@ninevastudios.com>
2019-11-11 11:11:45 +0200

    SDK-85 Change sandbox setting default value for PayStation

--- Source/XsollaPayStation/Private/XsollaPayStationSettings.cpp
+++ Source/XsollaPayStation/Private/XsollaPayStationSettings.cpp
@@ -6,6 +6,6 @@ UXsollaPayStationSettings::UXsollaPayStationSettings(const FObjectInitializer& O
 	: Super(ObjectInitializer)
 {
 	TokenRequestURL = TEXT("https://livedemo.xsolla.com/paystation/token_unreal.php");
-	bSandbox = true;
+	bSandbox = false;
 	bEnableSandboxInShipping = false;
 }
