3da31f93f27950862e7ef03899b2b2b85bd20e4b
Ivan Tustanivskyi <itustanivsky@ninevastudios.com>
2020-05-20 14:44:25 +0300

    SDK-529 Fix settings parameter tooltip

--- Source/XsollaStore/Public/XsollaStoreSettings.h
+++ Source/XsollaStore/Public/XsollaStoreSettings.h
@@ -68,7 +68,7 @@ public:
 	UPROPERTY(Config, EditAnywhere, BlueprintReadOnly, Category = "Xsolla Store Settings")
 	bool BuildForSteam;
 
-	/** If enabled, Login SDK will imitate platform-specific requests so you can try account linking from different platforms. */
+	/** If enabled, Store SDK will imitate platform-specific requests so you can try account linking from different platforms. */
 	UPROPERTY(Config, EditAnywhere, BlueprintReadOnly, DisplayName = "Use Cross-Platform Account Linking", Category = "Xsolla Store Settings")
 	bool UseCrossPlatformAccountLinking;
 
