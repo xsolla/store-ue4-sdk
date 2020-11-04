46ab36b7086b17d37be92588871601766782ecf0
Alexey Uporov <69147781+AlexeyUporov@users.noreply.github.com>
2020-11-04 13:29:52 +0500

    Update XsollaLogin.h

--- Source/XsollaLogin/Public/XsollaLogin.h
+++ Source/XsollaLogin/Public/XsollaLogin.h
@@ -21,9 +21,9 @@ public:
 
 	/**
 	 * Singleton-like access to this module's interface.  This is just for convenience!
-	 * Beware of calling this during the shutdown phase, though.  Your module might have been unloaded already.
+	 * Beware of calling this during the shutdown phase.  Your module might be unloaded already.
 	 *
-	 * @return Returns singleton instance, loading the module on demand if needed
+	 * @return Returns a singleton instance, loading the module on demand if needed.
 	 */
 	static inline FXsollaLoginModule& Get()
 	{
@@ -33,7 +33,7 @@ public:
 	/**
 	 * Checks to see if this module is loaded and ready.  It is only valid to call Get() if IsAvailable() returns true.
 	 *
-	 * @return True if the module is loaded and ready to use
+	 * @return True if the module is loaded and ready to use.
 	 */
 	static inline bool IsAvailable()
 	{
