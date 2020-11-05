c620dc86006f6144a524f946abb7237637fa5bf9
l.ratanov <l.ratanov@xsolla.com>
2020-11-05 15:36:23 +0500

    Update XsollaLoginLibrary.h
    вычитка комментов

--- Source/XsollaLogin/Public/XsollaLoginLibrary.h
+++ Source/XsollaLogin/Public/XsollaLoginLibrary.h
@@ -17,7 +17,7 @@ class XSOLLALOGIN_API UXsollaLoginLibrary : public UBlueprintFunctionLibrary
 	GENERATED_UCLASS_BODY()
 
 public:
-	/** Gives direct access to login SDK settings. */
+	/** Gives direct access to the login SDK settings. */
 	UFUNCTION(BlueprintPure, Category = "Xsolla|Login")
 	static UXsollaLoginSettings* GetLoginSettings();
 
@@ -29,19 +29,19 @@ public:
 	UFUNCTION(BlueprintPure, Category = "Xsolla|Library")
 	static FString GetStringCommandLineParam(const FString& ParamName);
 
-	/** Gets the platform specific authentication session ticket to verify user identity. */
+	/** Gets the specific platform authentication session ticket to verify user's identity. */
 	UFUNCTION(BlueprintPure, Category = "Xsolla|Library")
 	static FString GetSessionTicket();
 
-	/** Opens specified URL in the platfrom browser. */
+	/** Opens the specified URL in a platfrom browser. */
 	UFUNCTION(BlueprintCallable, Category = "Xsolla|Library")
 	static void LaunchPlatfromBrowser(const FString& URL);
 
-	/** Converts the 2D texture to its PNG-encoded byte representation. */
+	/** Converts a 2D texture into its PNG-encoded byte representation. */
 	UFUNCTION(BlueprintCallable, Category = "Xsolla|Library")
 	static TArray<uint8> ConvertTextureToByteArray(UTexture2D* Texture);
 
-	/** Gets thd specified parameter from a given URL. */
+	/** Gets the specified parameter from a given URL. */
 	UFUNCTION(BlueprintPure, Category = "Xsolla|Library")
 	static FString GetUrlParameter(const FString& URL, const FString& Parameter);
 };
