1ac3b3d29019b8637d72dc3340a53d0fb07ef856
Alexey Uporov <69147781+AlexeyUporov@users.noreply.github.com>
2020-11-11 14:09:49 +0500

    Update XsollaWebBrowserModule.h

--- Source/XsollaWebBrowser/Public/XsollaWebBrowserModule.h
+++ Source/XsollaWebBrowser/Public/XsollaWebBrowserModule.h
@@ -11,7 +11,7 @@ class IXsollaWebBrowserModule : public IModuleInterface
 public:
 	/**
 	 * Singleton-like access to this module's interface.  This is just for convenience!
-	 * Beware of calling this during the shutdown phase, though.  Your module might have been unloaded already.
+	 * Beware of calling this during the shutdown phase, though.  Your module might be already unloaded.
 	 *
 	 * @return Returns singleton instance, loading the module on demand if needed
 	 */
@@ -23,7 +23,7 @@ public:
 	/**
 	 * Checks to see if this module is loaded and ready.  It is only valid to call Get() if IsAvailable() returns true.
 	 *
-	 * @return True if the module is loaded and ready to use
+	 * @return True if the module is loaded and ready to use.
 	 */
 	static inline bool IsAvailable()
 	{
