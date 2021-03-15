3b827af03b61e67061412b63986ac25342759687
Ivan Tustanivskyi <itustanivsky@ninevastudios.com>
2021-03-15 14:11:26 +0200

    SDK-985 Add methods for request result processing

--- Source/XsollaUtils/Private/XsollaUtilsHttpRequestHelper.cpp
+++ Source/XsollaUtils/Private/XsollaUtilsHttpRequestHelper.cpp
@@ -3,7 +3,11 @@
 #include "XsollaUtilsHttpRequestHelper.h"
 #include "XsollaUtilsDefines.h"
 
+#include "Dom/JsonObject.h"
+#include "JsonObjectConverter.h"
 #include "Runtime/Launch/Resources/Version.h"
+#include "Serialization/JsonReader.h"
+#include "Serialization/JsonSerializer.h"
 
 TSharedRef<IHttpRequest, ESPMode::ThreadSafe> XsollaUtilsHttpRequestHelper::CreateHttpRequest(const FString& Url,
 	const EXsollaHttpRequestVerb Verb,
@@ -66,4 +70,159 @@ TSharedRef<IHttpRequest, ESPMode::ThreadSafe> XsollaUtilsHttpRequestHelper::Crea
 	}
 
 	return HttpRequest;
+}
+
+bool XsollaUtilsHttpRequestHelper::ParseResponse(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded, XsollaHttpRequestError& OutError)
+{
+	if (bSucceeded && HttpResponse.IsValid())
+	{
+		if (EHttpResponseCodes::IsOk(HttpResponse->GetResponseCode()))
+		{
+			return true;
+		}
+		else
+		{
+			FString ResponseStr = HttpResponse->GetContentAsString();
+
+			TSharedPtr<FJsonObject> JsonObject;
+			const TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(*ResponseStr);
+
+			if (FJsonSerializer::Deserialize(Reader, JsonObject) && JsonObject.IsValid())
+			{
+				if (!ParseError(JsonObject, OutError))
+				{
+					OutError = XsollaHttpRequestError("Unknown error");
+				}
+			}
+			else
+			{
+				OutError = XsollaHttpRequestError("Failed to deserialize response");
+			}
+		}
+	}
+	else
+	{
+		OutError = XsollaHttpRequestError("No response");
+	}
+
+	return false;
+}
+
+bool XsollaUtilsHttpRequestHelper::ParseResponseAsJson(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded, TSharedPtr<FJsonObject>& OutResponse, XsollaHttpRequestError& OutError)
+{
+	if (bSucceeded && HttpResponse.IsValid())
+	{
+		FString ResponseStr = HttpResponse->GetContentAsString();
+
+		TSharedPtr<FJsonObject> JsonObject;
+		const TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(*ResponseStr);
+
+		if (FJsonSerializer::Deserialize(Reader, JsonObject) && JsonObject.IsValid())
+		{
+			if (EHttpResponseCodes::IsOk(HttpResponse->GetResponseCode()))
+			{
+				OutResponse = JsonObject;
+				return true;
+			}
+			else
+			{
+				if (!ParseError(JsonObject, OutError))
+				{
+					OutError = XsollaHttpRequestError("Unknown error");
+				}
+			}
+		}
+		else
+		{
+			OutError = XsollaHttpRequestError("Failed to deserialize response");
+		}
+	}
+	else
+	{
+		OutError = XsollaHttpRequestError("No response");
+	}
+
+	return false;
+}
+
+bool XsollaUtilsHttpRequestHelper::ParseResponseAsStruct(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded, const UStruct* OutResponseDefinition, void* OutResponse, XsollaHttpRequestError& OutError)
+{
+	TSharedPtr<FJsonObject> JsonObject;
+	if (ParseResponseAsJson(HttpRequest, HttpResponse, bSucceeded, JsonObject, OutError))
+	{
+		if (FJsonObjectConverter::JsonObjectToUStruct(JsonObject.ToSharedRef(), OutResponseDefinition, OutResponse))
+		{
+			return true;
+		}
+
+		OutError = XsollaHttpRequestError("Failed to convert response");
+	}
+
+	return false;
+}
+
+template <typename OutStructType>
+bool XsollaUtilsHttpRequestHelper::ParseResponseAsArray(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded, TArray<OutStructType>* OutResponse, XsollaHttpRequestError& OutError)
+{
+	if (bSucceeded && HttpResponse.IsValid())
+	{
+		FString ResponseStr = HttpResponse->GetContentAsString();
+
+		if (EHttpResponseCodes::IsOk(HttpResponse->GetResponseCode()))
+		{
+			if (FJsonObjectConverter::JsonArrayStringToUStruct(ResponseStr, OutResponse, 0, 0))
+			{
+				return true;
+			}
+
+			OutError = XsollaHttpRequestError("Failed to convert response");
+		}
+		else
+		{
+			TSharedPtr<FJsonObject> JsonObject;
+			const TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(*ResponseStr);
+
+			if (FJsonSerializer::Deserialize(Reader, JsonObject) && JsonObject.IsValid())
+			{
+				if (!ParseError(JsonObject, OutError))
+				{
+					OutError = XsollaHttpRequestError("Unknown error");
+				}
+			}
+			else
+			{
+				OutError = XsollaHttpRequestError("Failed to deserialize response");
+			}
+		}
+	}
+	else
+	{
+		OutError = XsollaHttpRequestError("No response");
+	}
+
+	return false;
+}
+
+bool XsollaUtilsHttpRequestHelper::ParseError(TSharedPtr<FJsonObject> JsonObject, XsollaHttpRequestError& OutError)
+{
+	// Try parse Login API error
+	if (JsonObject->HasTypedField<EJson::Object>(TEXT("error")))
+	{
+		TSharedPtr<FJsonObject> ErrorObject = JsonObject.Get()->GetObjectField(TEXT("error"));
+		OutError.code = ErrorObject.Get()->GetStringField(TEXT("code"));
+		OutError.description = ErrorObject.Get()->GetStringField(TEXT("description"));
+		return true;
+	}
+
+	// Try parse Commerce API error
+	if (JsonObject->HasTypedField<EJson::String>(TEXT("errorMessage")))
+	{
+		OutError.statusCode = JsonObject->GetNumberField(TEXT("statusCode"));
+		OutError.errorCode = JsonObject->GetNumberField(TEXT("errorCode"));
+		OutError.errorMessage = JsonObject->GetStringField(TEXT("errorMessage"));
+		return true;
+	}
+
+	// No error
+	return false;
 }
--- Source/XsollaUtils/Public/XsollaUtilsHttpRequestHelper.h
+++ Source/XsollaUtils/Public/XsollaUtilsHttpRequestHelper.h
@@ -5,6 +5,8 @@
 #include "CoreMinimal.h"
 #include "Http.h"
 
+class FJsonObject;
+
 enum class EXsollaHttpRequestVerb : uint8
 {
 	VERB_GET,
@@ -14,14 +16,41 @@ enum class EXsollaHttpRequestVerb : uint8
 	VERB_PATCH
 };
 
+struct XSOLLAUTILS_API XsollaHttpRequestError
+{
+	FString code;
+	FString description;
+	FString errorMessage;
+	int32 statusCode;
+	int32 errorCode;
+
+	XsollaHttpRequestError()
+	{
+	}
+
+	XsollaHttpRequestError(const FString& errorMsg)
+		: description(errorMsg)
+		, errorMessage(errorMsg)
+	{
+	}
+};
+
 class XSOLLAUTILS_API XsollaUtilsHttpRequestHelper
 {
 public:
-
 	static TSharedRef<IHttpRequest, ESPMode::ThreadSafe> CreateHttpRequest(const FString& Url,
 		const EXsollaHttpRequestVerb Verb = EXsollaHttpRequestVerb::VERB_GET,
 		const FString& AuthToken = FString(),
 		const FString& Content = FString(),
 		const FString& SdkModuleName = FString(),
 		const FString& SdkModuleVersion = FString());
+
+	static bool ParseResponse(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded, XsollaHttpRequestError& OutError);
+	static bool ParseResponseAsJson(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded, TSharedPtr<FJsonObject>& OutResponse, XsollaHttpRequestError& OutError);
+	static bool ParseResponseAsStruct(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded, const UStruct* OutResponseDefinition, void* OutResponse, XsollaHttpRequestError& OutError);
+
+	template <typename OutStructType>
+	static bool ParseResponseAsArray(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded, TArray<OutStructType>* OutResponse, XsollaHttpRequestError& OutError);
+
+	static bool ParseError(TSharedPtr<FJsonObject> JsonObject, XsollaHttpRequestError& OutError);
 };
