0cc7981fc7534569a05c8a62f23ca48ab4b78138
Ivan Tustanivskyi <tustanivsky@gmail.com>
2021-08-05 16:22:25 +0300

    SDK-1535 Add utility class to resolve issues with world context for classes derived from UObject

---
+++ Source/XsollaUtils/Private/XsollaWorldObject.cpp
@@ -0,0 +1,3 @@
+// Copyright 2021 Xsolla Inc. All Rights Reserved.
+
+#include "XsollaWorldObject.h"
---
+++ Source/XsollaUtils/Public/XsollaWorldObject.h
@@ -0,0 +1,15 @@
+// Copyright 2021 Xsolla Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "XsollaWorldObject.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, meta = (ShowWorldContextPin))
+class XSOLLAUTILS_API UXsollaWorldObject : public UObject
+{
+	GENERATED_BODY()
+};
