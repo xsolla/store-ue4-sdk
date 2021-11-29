11e25c216b745dfab68614a564aff427ad116bbe
lelka <salinaolga@gmail.com>
2021-11-29 16:54:58 +0300

    SDK-2010 Update plugin version

--- Source/XsollaLogin/Private/XsollaLoginDefines.h
+++ Source/XsollaLogin/Private/XsollaLoginDefines.h
@@ -13,4 +13,4 @@ DECLARE_LOG_CATEGORY_EXTERN(LogXsollaLogin, Log, All);
 #define VA_LINE (FString::FromInt(__LINE__))		 // Current Line Number in the code where this is called
 #define VA_FUNC_LINE (VA_FUNC + "(" + VA_LINE + ")") // Current Class and Line Number where this is called!
 
-#define XSOLLA_LOGIN_VERSION TEXT("0.7.0")
+#define XSOLLA_LOGIN_VERSION TEXT("0.7.1")
