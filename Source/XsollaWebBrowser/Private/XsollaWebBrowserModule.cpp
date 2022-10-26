fef154e58f02fe317089cbf59a106044df980a54
Olga Salina <o.salina@xsolla.com>
2022-10-26 17:45:03 +0300

    SDK-3074 Remove custom WebBrowser settings

--- Source/XsollaWebBrowser/Private/XsollaWebBrowserModule.cpp
+++ Source/XsollaWebBrowser/Private/XsollaWebBrowserModule.cpp
@@ -18,13 +18,6 @@ void FXsollaWebBrowserModule::StartupModule()
 		WebBrowserAssetMgr = NewObject<UXsollaWebBrowserAssetManager>((UObject*)GetTransientPackage(), NAME_None, RF_Transient | RF_Public);
 		WebBrowserAssetMgr->LoadDefaultMaterials();
 
-		FWebBrowserInitSettings WebBrowserInitSettings;
-
-		FString ChromeVersion = TEXT("Chrome/87.0.4280.66");
-
-		WebBrowserInitSettings.ProductVersion = FString::Printf(TEXT("%s/%s UnrealEngine/%s Mozilla/5.0 (Linux; Android 10; Redmi Note 7) AppleWebKit/537.36 (KHTML, like Gecko) %s Safari/537.36"), FApp::GetProjectName(), FApp::GetBuildVersion(), *FEngineVersion::Current().ToString(), *ChromeVersion);
-		IWebBrowserModule::Get().CustomInitialize(WebBrowserInitSettings);
-
 		IWebBrowserSingleton* WebBrowserSingleton = IWebBrowserModule::Get().GetSingleton();
 		if (WebBrowserSingleton)
 		{
