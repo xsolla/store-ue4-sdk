87c7896ba9371d06a2c799475a5ab5207ea8dac8
Denis Rakhimov <d.rakhimov@xsolla.com>
2023-06-01 14:29:14 +0800

    Another fix of copy files command

--- cicd/build-demo-android.bat
+++ cicd/build-demo-android.bat
@@ -21,9 +21,9 @@ echo ### DEFINE VARIABLES FOR PACKAGE TEST PROJECT ###
 
 set AT_PATH="C:\EpicGames\UE_%ENGINE_VERSION%\Engine\Binaries\DotNET\AutomationTool.exe"
 if %MAJOR_VERSION%==5 (set AT_PATH="C:\EpicGames\UE_%ENGINE_VERSION%\Engine\Binaries\DotNET\AutomationTool\AutomationTool.exe")
-if %MAJOR_VERSION%==5 (copy /Y "%CI_WORK_DIR%\cicd\project.properties" "%PROJECT_DIR%\Build\Android\project.properties")
-if %MAJOR_VERSION%==4 (copy /Y "%CI_WORK_DIR%\cicd\UserEngine-4.ini" "C:\Users\Runner\AppData\Local\Unreal Engine\Engine\Config\UserEngine.ini")
-if %MAJOR_VERSION%==5 (copy /Y "%CI_WORK_DIR%\cicd\UserEngine-5.ini" "C:\Users\Runner\AppData\Local\Unreal Engine\Engine\Config\UserEngine.ini")
+if %MAJOR_VERSION%==5 (xcopy /Y /S /F "%CI_WORK_DIR%\cicd\project.properties" "%PROJECT_DIR%\Build\Android\project.properties*")
+if %MAJOR_VERSION%==4 (xcopy /Y /S /F "%CI_WORK_DIR%\cicd\UserEngine-4.ini" "C:\Users\Runner\AppData\Local\Unreal Engine\Engine\Config\UserEngine.ini*")
+if %MAJOR_VERSION%==5 (xcopy /Y /S /F "%CI_WORK_DIR%\cicd\UserEngine-5.ini" "C:\Users\Runner\AppData\Local\Unreal Engine\Engine\Config\UserEngine.ini*")
 
 set UPROJECT_PATH=%PROJECT_DIR%\%PROJECT_NAME%.uproject
 set PACKAGE_ROOT_DIR=%CI_WORK_DIR%\Builds
