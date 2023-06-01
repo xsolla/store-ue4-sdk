2b1ba02178bef8a7e76329c8143b654dcc82a9d0
Denis Rakhimov <d.rakhimov@xsolla.com>
2023-06-01 11:08:16 +0800

    test android builds

--- cicd/build-demo-android.bat
+++ cicd/build-demo-android.bat
@@ -4,7 +4,9 @@ set PROJECT_DIR=%1
 set PROJECT_BRANCH=%2
 set PLUGIN_BRANCH=%3
 set CI_WORK_DIR=%4
+set ENGINE_VERSION=%5
 set PROJECT_NAME=StoreUeSdkTest
+set MAJOR_VERSION=%ENGINE_VERSION:~0,1%
 
 echo.
 echo ===========================
@@ -17,7 +19,9 @@ echo.
 echo ============================
 echo ### PACKAGE TEST PROJECT ###
 
-set AT_PATH="C:\EpicGames\UE_4.27\Engine\Binaries\DotNET\AutomationTool.exe"
+set AT_PATH="C:\EpicGames\UE_%ENGINE_VERSION%\Engine\Binaries\DotNET\AutomationTool.exe"
+if %MAJOR_VERSION%==5 (set AT_PATH="C:\EpicGames\UE_%ENGINE_VERSION%\Engine\Binaries\DotNET\AutomationTool\AutomationTool.exe")
+
 set UPROJECT_PATH=%PROJECT_DIR%\%PROJECT_NAME%.uproject
 set PACKAGE_ROOT_DIR=%CI_WORK_DIR%\Builds
 set SETTINGS_INI_PATH=%PROJECT_DIR%\Config\DefaultEngine.ini
