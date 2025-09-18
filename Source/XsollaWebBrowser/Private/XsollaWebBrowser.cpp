e650fd2adc3df14190056d46ec61d326a6e4cefd
Aleksei Demonov <a.demonov@xsolla.com>
2025-09-18 13:10:17 +0400

    SDK-4540 Add additional logging for Browser module

--- Source/XsollaWebBrowser/Private/XsollaWebBrowser.cpp
+++ Source/XsollaWebBrowser/Private/XsollaWebBrowser.cpp
@@ -2,6 +2,7 @@
 
 #include "XsollaWebBrowser.h"
 
+#include "XsollaWebBrowserDefines.h"
 #include "Async/TaskGraphInterfaces.h"
 #include "IWebBrowserCookieManager.h"
 #include "IWebBrowserSingleton.h"
@@ -20,25 +21,43 @@ UXsollaWebBrowser::UXsollaWebBrowser(const FObjectInitializer& ObjectInitializer
 
 void UXsollaWebBrowser::LoadURL(FString NewURL)
 {
+	UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: Loading URL: %s"), *VA_FUNC_LINE, *NewURL);
+
 	if (WebBrowserWidget.IsValid())
 	{
-		return WebBrowserWidget->LoadURL(NewURL);
+		WebBrowserWidget->LoadURL(NewURL);
+	}
+	else
+	{
+		UE_LOG(LogXsollaWebBrowser, Warning, TEXT("%s: WebBrowserWidget is not valid"), *VA_FUNC_LINE);
 	}
 }
 
 void UXsollaWebBrowser::LoadHtml(FString Contents, FString DummyURL)
 {
+	UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: Loading HTML with dummy URL: %s"), *VA_FUNC_LINE, *DummyURL);
+
 	if (WebBrowserWidget.IsValid())
 	{
-		return WebBrowserWidget->LoadString(Contents, DummyURL);
+		WebBrowserWidget->LoadString(Contents, DummyURL);
+	}
+	else
+	{
+		UE_LOG(LogXsollaWebBrowser, Warning, TEXT("%s: WebBrowserWidget is not valid"), *VA_FUNC_LINE);
 	}
 }
 
 void UXsollaWebBrowser::ExecuteJavascript(FString ScriptText)
 {
+	UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: Executing JavaScript"), *VA_FUNC_LINE);
+
 	if (WebBrowserWidget.IsValid())
 	{
-		return WebBrowserWidget->ExecuteJavascript(ScriptText);
+		WebBrowserWidget->ExecuteJavascript(ScriptText);
+	}
+	else
+	{
+		UE_LOG(LogXsollaWebBrowser, Warning, TEXT("%s: WebBrowserWidget is not valid"), *VA_FUNC_LINE);
 	}
 }
 
@@ -46,14 +65,19 @@ FString UXsollaWebBrowser::GetUrl() const
 {
 	if (WebBrowserWidget.IsValid())
 	{
-		return WebBrowserWidget->GetUrl();
+		FString CurrentUrl = WebBrowserWidget->GetUrl();
+		UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: Current URL: %s"), *VA_FUNC_LINE, *CurrentUrl);
+		return CurrentUrl;
 	}
 
+	UE_LOG(LogXsollaWebBrowser, Warning, TEXT("%s: WebBrowserWidget is not valid, returning empty URL"), *VA_FUNC_LINE);
 	return FString();
 }
 
 void UXsollaWebBrowser::ClearCache() const
 {
+	UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: Clearing browser cache"), *VA_FUNC_LINE);
+
 	IWebBrowserSingleton* WebBrowserSingleton = IWebBrowserModule::Get().GetSingleton();
 	if (WebBrowserSingleton)
 	{
@@ -61,38 +85,82 @@ void UXsollaWebBrowser::ClearCache() const
 		if (CookieManager.IsValid())
 		{
 			CookieManager->DeleteCookies();
+			UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: Browser cookies deleted"), *VA_FUNC_LINE);
 		}
+		else
+		{
+			UE_LOG(LogXsollaWebBrowser, Warning, TEXT("%s: CookieManager is not valid"), *VA_FUNC_LINE);
+		}
+	}
+	else
+	{
+		UE_LOG(LogXsollaWebBrowser, Warning, TEXT("%s: WebBrowserSingleton is not valid"), *VA_FUNC_LINE);
 	}
 }
 
 void UXsollaWebBrowser::GoBack() const
 {
-	if (WebBrowserWidget.IsValid() && WebBrowserWidget->CanGoBack())
+	UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: Attempting to navigate back"), *VA_FUNC_LINE);
+
+	if (WebBrowserWidget.IsValid())
 	{
-		return WebBrowserWidget->GoBack();
+		if (WebBrowserWidget->CanGoBack())
+		{
+			WebBrowserWidget->GoBack();
+			UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: Navigated back"), *VA_FUNC_LINE);
+		}
+		else
+		{
+			UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: Cannot go back - no history available"), *VA_FUNC_LINE);
+		}
+	}
+	else
+	{
+		UE_LOG(LogXsollaWebBrowser, Warning, TEXT("%s: WebBrowserWidget is not valid"), *VA_FUNC_LINE);
 	}
 }
 
 void UXsollaWebBrowser::GoForward() const
 {
-	if (WebBrowserWidget.IsValid() && WebBrowserWidget->CanGoForward())
+	UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: Attempting to navigate forward"), *VA_FUNC_LINE);
+
+	if (WebBrowserWidget.IsValid())
 	{
-		return WebBrowserWidget->GoForward();
+		if (WebBrowserWidget->CanGoForward())
+		{
+			WebBrowserWidget->GoForward();
+			UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: Navigated forward"), *VA_FUNC_LINE);
+		}
+		else
+		{
+			UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: Cannot go forward - no forward history available"), *VA_FUNC_LINE);
+		}
+	}
+	else
+	{
+		UE_LOG(LogXsollaWebBrowser, Warning, TEXT("%s: WebBrowserWidget is not valid"), *VA_FUNC_LINE);
 	}
 }
 
 void UXsollaWebBrowser::ReleaseSlateResources(bool bReleaseChildren)
 {
+	UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: Releasing slate resources"), *VA_FUNC_LINE);
+
 	Super::ReleaseSlateResources(bReleaseChildren);
 
 	WebBrowserWidget.Reset();
+
+	UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: WebBrowserWidget reset"), *VA_FUNC_LINE);
 }
 
 TSharedRef<SWidget> UXsollaWebBrowser::RebuildWidget()
 {
+	UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: Rebuilding widget"), *VA_FUNC_LINE);
+
 	// clang-format off
 	if (IsDesignTime())
 	{
+		UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: Creating design time widget"), *VA_FUNC_LINE);
 		return SNew(SBox)
 			.HAlign(HAlign_Center)
 			.VAlign(VAlign_Center)
@@ -103,6 +171,9 @@ TSharedRef<SWidget> UXsollaWebBrowser::RebuildWidget()
 	}
 	else
 	{
+		UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: Creating web browser widget with initial URL: %s"),
+			*VA_FUNC_LINE, *InitialURL);
+
 		WebBrowserWidget = SNew(SWebBrowser)
 			.InitialURL(InitialURL)
 			.ShowControls(false)
@@ -118,24 +189,31 @@ TSharedRef<SWidget> UXsollaWebBrowser::RebuildWidget()
 
 void UXsollaWebBrowser::HandleOnUrlChanged(const FText& InText)
 {
+	UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: URL changed to: %s"), *VA_FUNC_LINE, *InText.ToString());
 	OnUrlChanged.Broadcast(InText);
 }
 
 void UXsollaWebBrowser::HandleOnPageLoaded()
 {
+	UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: Page loaded"), *VA_FUNC_LINE);
 	OnPageLoaded.Broadcast();
 }
 
 bool UXsollaWebBrowser::HandleOnBeforePopup(FString URL, FString Frame)
 {
+	UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: Before popup event - URL: %s, Frame: %s"),
+		*VA_FUNC_LINE, *URL, *Frame);
+
 	if (OnBeforePopup.IsBound())
 	{
 		if (IsInGameThread())
 		{
+			UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: Broadcasting OnBeforePopup event on game thread"), *VA_FUNC_LINE);
 			OnBeforePopup.Broadcast(URL, Frame);
 		}
 		else
 		{
+			UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: Dispatching OnBeforePopup event to game thread"), *VA_FUNC_LINE);
 			// clang-format off
 			// Retry on the GameThread now
 			TWeakObjectPtr<UXsollaWebBrowser> WeakThis = this;
@@ -152,6 +230,7 @@ bool UXsollaWebBrowser::HandleOnBeforePopup(FString URL, FString Frame)
 		return true;
 	}
 
+	UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: No OnBeforePopup delegate bound"), *VA_FUNC_LINE);
 	return false;
 }
 
