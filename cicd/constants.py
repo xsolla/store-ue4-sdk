38c609277c2c113396c02ac236e7e7831052fde1
Aleksei Demonov <a.demonov@xsolla.com>
2025-11-11 10:17:51 +0400

    SDK-4589 Return demo 'master' branch

--- cicd/constants.py
+++ cicd/constants.py
@@ -1,5 +1,5 @@
 demo_project_remote = 'git@gitlab.loc:sdk_group/store-ue-sdk-test.git'
-demo_project_branch = 'feature/SDK-4589'
+demo_project_branch = 'master'
 demo_project_name = 'StoreUeSdkTest'
 
 plugin_remote = 'git@gitlab.loc:sdk_group/store-ue4-sdk.git'
