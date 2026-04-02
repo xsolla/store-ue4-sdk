db9b5fca4f92bfb90973185bbed2823868ee4e9d
Aleksei Demonov <a.demonov@xsolla.com>
2026-04-02 14:45:56 +0400

    SDK-4868 Mitigate UE SDK issues when paying via Google Pay
    + Attempt to use GooglePay now leads to external browser
    + Additional logging for in-built browser pop-ups

--- Source/XsollaWebBrowser/Private/XsollaWebBrowser.cpp
+++ Source/XsollaWebBrowser/Private/XsollaWebBrowser.cpp
@@ -1,4 +1,4 @@
-﻿// Copyright 2024 Xsolla Inc. All Rights Reserved.
+// Copyright 2024 Xsolla Inc. All Rights Reserved.
 
 #include "XsollaWebBrowser.h"
 
@@ -13,6 +13,85 @@
 #include "XsollaUtilsLoggingHelper.h"
 
 #define LOCTEXT_NAMESPACE "XsollaWebBrowser"
+
+namespace
+{
+	enum class EXsollaPopupSchemeType : uint8
+	{
+		Empty,
+		Http,
+		Https,
+		About,
+		Javascript,
+		Other
+	};
+
+	EXsollaPopupSchemeType ClassifyPopupScheme(const FString& Url, FString& OutScheme)
+	{
+		const FString TrimmedUrl = Url.TrimStartAndEnd();
+		if (TrimmedUrl.IsEmpty())
+		{
+			OutScheme = TEXT("empty");
+			return EXsollaPopupSchemeType::Empty;
+		}
+
+		int32 SchemeSeparatorIdx = INDEX_NONE;
+		if (!TrimmedUrl.FindChar(TEXT(':'), SchemeSeparatorIdx) || SchemeSeparatorIdx <= 0)
+		{
+			OutScheme = TEXT("none");
+			return EXsollaPopupSchemeType::Other;
+		}
+
+		OutScheme = TrimmedUrl.Left(SchemeSeparatorIdx).ToLower();
+		if (OutScheme == TEXT("http"))
+		{
+			return EXsollaPopupSchemeType::Http;
+		}
+		if (OutScheme == TEXT("https"))
+		{
+			return EXsollaPopupSchemeType::Https;
+		}
+		if (OutScheme == TEXT("about"))
+		{
+			return EXsollaPopupSchemeType::About;
+		}
+		if (OutScheme == TEXT("javascript"))
+		{
+			return EXsollaPopupSchemeType::Javascript;
+		}
+
+		return EXsollaPopupSchemeType::Other;
+	}
+
+	const TCHAR* PopupSchemeTypeToString(const EXsollaPopupSchemeType SchemeType)
+	{
+		switch (SchemeType)
+		{
+		case EXsollaPopupSchemeType::Empty:
+			return TEXT("Empty");
+		case EXsollaPopupSchemeType::Http:
+			return TEXT("Http");
+		case EXsollaPopupSchemeType::Https:
+			return TEXT("Https");
+		case EXsollaPopupSchemeType::About:
+			return TEXT("About");
+		case EXsollaPopupSchemeType::Javascript:
+			return TEXT("Javascript");
+		default:
+			return TEXT("Other");
+		}
+	}
+
+	bool ShouldRoutePopupToExternalHandler(const EXsollaPopupSchemeType SchemeType)
+	{
+		return SchemeType == EXsollaPopupSchemeType::Http || SchemeType == EXsollaPopupSchemeType::Https;
+	}
+
+	bool IsExternalPaystationPopupFrame(const FString& FrameName)
+	{
+		return FrameName.StartsWith(TEXT("PayStationExternalPayment"), ESearchCase::IgnoreCase);
+	}
+}
 
 UXsollaWebBrowser::UXsollaWebBrowser(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
@@ -209,19 +288,60 @@ void UXsollaWebBrowser::HandleOnPageLoaded()
 bool UXsollaWebBrowser::HandleOnBeforePopup(FString URL, FString Frame)
 {
 	FString SanitizedUrl = XsollaUtilsLoggingHelper::SanitizeUrlForLogging(URL);
-	UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: Before popup event - URL: %s, Frame: %s"),
-		*VA_FUNC_LINE, *SanitizedUrl, *Frame);
+	FString Scheme;
+	const EXsollaPopupSchemeType SchemeType = ClassifyPopupScheme(URL, Scheme);
+	UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: XS_POPUP_TRACE before popup - URL: %s, Frame: %s, Scheme: %s, SchemeType: %s"),
+		*VA_FUNC_LINE, *SanitizedUrl, *Frame, *Scheme, PopupSchemeTypeToString(SchemeType));
+
+	if (!ShouldRoutePopupToExternalHandler(SchemeType))
+	{
+		// Paystation can bootstrap external payment windows with about:blank.
+		// In embedded mode this popup does not progress, so route to external browser via existing delegate using current URL.
+		if (OnBeforePopup.IsBound() && IsExternalPaystationPopupFrame(Frame) && WebBrowserWidget.IsValid())
+		{
+			const FString FallbackUrl = WebBrowserWidget->GetUrl();
+			const bool bHasHttpFallbackUrl = FallbackUrl.StartsWith(TEXT("http://"), ESearchCase::IgnoreCase)
+				|| FallbackUrl.StartsWith(TEXT("https://"), ESearchCase::IgnoreCase);
+			if (bHasHttpFallbackUrl)
+			{
+				const FString SanitizedFallbackUrl = XsollaUtilsLoggingHelper::SanitizeUrlForLogging(FallbackUrl);
+				UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: XS_POPUP_TRACE decision=external_fallback_current_paystation_url frame=%s fallback_url=%s"),
+					*VA_FUNC_LINE, *Frame, *SanitizedFallbackUrl);
+
+				if (IsInGameThread())
+				{
+					OnBeforePopup.Broadcast(FallbackUrl, Frame);
+				}
+				else
+				{
+					TWeakObjectPtr<UXsollaWebBrowser> WeakThis = this;
+					FFunctionGraphTask::CreateAndDispatchWhenReady([WeakThis, FallbackUrl, Frame]()
+					{
+						if (WeakThis.IsValid() && WeakThis->OnBeforePopup.IsBound())
+						{
+							WeakThis->OnBeforePopup.Broadcast(FallbackUrl, Frame);
+						}
+					}, TStatId(), nullptr, ENamedThreads::GameThread);
+				}
+
+				return true;
+			}
+		}
+
+		UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: XS_POPUP_TRACE decision=allow_embedded_popup (non-http(s) scheme)"), *VA_FUNC_LINE);
+		return false;
+	}
 
 	if (OnBeforePopup.IsBound())
 	{
 		if (IsInGameThread())
 		{
-			UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: Broadcasting OnBeforePopup event on game thread"), *VA_FUNC_LINE);
+			UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: XS_POPUP_TRACE decision=external_handler thread=game"), *VA_FUNC_LINE);
 			OnBeforePopup.Broadcast(URL, Frame);
 		}
 		else
 		{
-			UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: Dispatching OnBeforePopup event to game thread"), *VA_FUNC_LINE);
+			UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: XS_POPUP_TRACE decision=dispatch_to_game_thread"), *VA_FUNC_LINE);
 			// clang-format off
 			// Retry on the GameThread now
 			TWeakObjectPtr<UXsollaWebBrowser> WeakThis = this;
@@ -238,7 +358,7 @@ bool UXsollaWebBrowser::HandleOnBeforePopup(FString URL, FString Frame)
 		return true;
 	}
 
-	UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: No OnBeforePopup delegate bound"), *VA_FUNC_LINE);
+	UE_LOG(LogXsollaWebBrowser, Log, TEXT("%s: XS_POPUP_TRACE no external delegate bound, fallback=embedded"), *VA_FUNC_LINE);
 	return false;
 }
 
